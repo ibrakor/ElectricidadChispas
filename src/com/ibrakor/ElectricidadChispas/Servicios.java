24f35d0746134e294f4465e869d0e89979b3390e
Brahim Korsan El Ghabi <104716025+ibrakor@users.noreply.github.com>
2022-11-24 18:53:59 +0100

    Terminada clase Servicios

--- src/com/ibrakor/ElectricidadChispas/Servicios.java
+++ src/com/ibrakor/ElectricidadChispas/Servicios.java
@@ -1,8 +1,48 @@
 package com.ibrakor.ElectricidadChispas;
 
-public class Servicios {
+public class Servicios implements Vendible {
     private Integer codservicio;
     private String nombre;
     private Integer precio;
     private Integer tipoiva;
+
+    @Override
+    public Integer getCodproducto() {
+        return codservicio;
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public Integer getCodservicio() {
+        return codservicio;
+    }
+
+    public void setCodservicio(Integer codservicio) {
+        this.codservicio = codservicio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    public void setTipoiva(Integer tipoiva) {
+        this.tipoiva = tipoiva;
+    }
+
+    @Override
+    public Integer getTipoiva() {
+        return tipoiva;
+    }
 }
