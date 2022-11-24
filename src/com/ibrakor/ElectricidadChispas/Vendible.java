d938a4f5c6f1e0d376b2d4f6baa64b24bf108eab
Brahim Korsan El Ghabi <104716025+ibrakor@users.noreply.github.com>
2022-11-24 18:53:20 +0100

    Terminada Interfaz para los productos y Servicios

---
+++ src/com/ibrakor/ElectricidadChispas/Vendible.java
@@ -0,0 +1,8 @@
+package com.ibrakor.ElectricidadChispas;
+
+public interface Vendible {
+    Integer getCodproducto();
+    String getNombre();
+    Integer getPrecio();
+    Integer getTipoiva();
+}
