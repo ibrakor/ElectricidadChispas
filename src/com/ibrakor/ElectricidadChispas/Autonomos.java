bb69851cb36a27ae4b7bc13be3f303faa28ea877
Brahim Korsan El Ghabi <104716025+ibrakor@users.noreply.github.com>
2022-11-24 15:35:00 +0100

    Corregidas clases

--- src/com/ibrakor/ElectricidadChispas/Autonomos.java
+++ src/com/ibrakor/ElectricidadChispas/Autonomos.java
@@ -4,82 +4,9 @@ public class Autonomos implements Clientes {
     private Integer codcliente;
     private String nombre;
     private String apellido;
-    private Integer dni;
+    private String dni;
     private String direccion;
     private String poblacion;
-
-    public Integer getCodcliente() {
-        return codcliente;
-    }
-
-    public void setCodcliente(Integer codcliente) {
-        this.codcliente = codcliente;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public Integer getDni() {
-        return dni;
-    }
-
-    public void setDni(Integer dni) {
-        this.dni = dni;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getPoblacion() {
-        return poblacion;
-    }
-
-    public void setPoblacion(String poblacion) {
-        this.poblacion = poblacion;
-    }
-
-    public String getProvincia() {
-        return provincia;
-    }
-
-    public void setProvincia(String provincia) {
-        this.provincia = provincia;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(Integer telefono) {
-        this.telefono = telefono;
-    }
-
     private String provincia;
     private String email;
     private Integer telefono;
--- src/com/ibrakor/ElectricidadChispas/Sociedades.java
+++ src/com/ibrakor/ElectricidadChispas/Sociedades.java
@@ -3,73 +3,8 @@ package com.ibrakor.ElectricidadChispas;
 public class Sociedades implements Clientes {
     private Integer codcliente;
     private String razonsocial;
-    private Integer cif;
+    private String cif;
     private String direccion;
-
-    public Integer getCodcliente() {
-        return codcliente;
-    }
-
-    public void setCodcliente(Integer codcliente) {
-        this.codcliente = codcliente;
-    }
-
-    public String getRazonsocial() {
-        return razonsocial;
-    }
-
-    public void setRazonsocial(String razonsocial) {
-        this.razonsocial = razonsocial;
-    }
-
-    public Integer getCif() {
-        return cif;
-    }
-
-    public void setCif(Integer cif) {
-        this.cif = cif;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getPoblacion() {
-        return poblacion;
-    }
-
-    public void setPoblacion(String poblacion) {
-        this.poblacion = poblacion;
-    }
-
-    public String getProvincia() {
-        return provincia;
-    }
-
-    public void setProvincia(String provincia) {
-        this.provincia = provincia;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(Integer telefono) {
-        this.telefono = telefono;
-    }
-
     private String poblacion;
     private String provincia;
     private String email;
